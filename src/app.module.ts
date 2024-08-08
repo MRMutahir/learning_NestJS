da9ee1b6e1200c3e9ab39d84828034402a1fb3aa
MRMutahir <mutahirkareem820@gmail.com>
2024-08-08 18:28:32 +0500

    NestJS #13 - Dependency Injection | Factory Providers | Dynamic & Async Providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,21 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
-import { UsersStore } from './user.store';
+import { BehaviorSubject, ReplaySubject } from 'rxjs';
 
+const is_dev_mode = false;
 @Module({
-  // controllers: [UsersController, EducationController, AdminController, IP],
   controllers: [UsersController],
   providers: [
-    { provide: 'email', useValue: ['mutahir@gmail.com', '@gmail.com'] },
-  ], // Array value
-  // providers: [{ provide: 'DB_NAME', useValue: 'Muhammad Mutahir' }], // single value
-  // providers: [UsersStore], // short hand
-  // providers: [{ provide: UsersStore, useClass: UsersStore }],
+    {
+      provide: 'EVENT_STORE',
+      useFactory: () => {
+        const eventBus = is_dev_mode
+          ? new ReplaySubject(2)
+          : new BehaviorSubject(null);
+
+        return eventBus;
+      },
+    },
+  ],
 })
 export class AppModule {}
--- src/users.controller.ts
+++ src/users.controller.ts
@@ -1,84 +1,12 @@
-// import { Controller, Get, HttpCode, Post, Req } from '@nestjs/common';
-// // import { Request } from 'express';
+import { Controller, Get, Inject } from '@nestjs/common';
 
-import { Controller, Inject, Optional } from '@nestjs/common';
-import { UsersStore } from './user.store';
-
-// @Controller('/users')
-// export class UsersController {
-//   @Get('/profile')
-//   @HttpCode(204)
-//   getProfile(@Req() req) {
-//     return {
-//       message: 'SALAM to all of you!',
-//       description:
-//         'Welcome to the profile route of the UsersController. I am currently learning NestJS to enhance my backend development skills.',
-//       data: {
-//         name: 'Muhammad Mutahir',
-//         role: 'Web Developer',
-//         technologies: [
-//           'MERN',
-//           'Node',
-//           'Express',
-//           'MongoDB',
-//           'React',
-//           'JavaScript',
-//           'Next.js',
-//           'Sanity',
-//           'HTML',
-//           'CSS',
-//           'Tailwind',
-//           'Firebase',
-//           'Redux',
-//           'Figma',
-//           'TypeScript',
-//         ],
-//         learning: 'NestJS',
-//         currentEmploy: {
-//           company: 'gr-tech',
-//           jobRole: 'Backend Developer',
-//         },
-//       },
-//     };
-//   }
-// }
-
-// both way are correct
-// @Controller()
-// export class UsersController {
-//   constructor(private readonly store: UsersStore) {
-//     console.log('this.store', this.store);
-//   }
-// }
-
-// @Controller()
-// export class UsersController {
-//   constructor(@Optional()private readonly store: UsersStore) {
-//     console.log('this.store', this.store);
-//   }
-// }
-// @Controller()
-// export class UsersController {
-//   constructor(@Inject(UsersStore) private readonly store: UsersStore) {
-//     console.log('this.store', this.store);
-//   }
-// }
-
-// Single Value
-
-// @Controller()
-// export class UsersController {
-//   constructor(@Inject('DB_NAME') private  dbName) {
-//     console.log('this.store', this.dbName);
-//   }
-// }
-
-
-// Array Value
-
-@Controller()
+@Controller('/users')
 export class UsersController {
-  constructor(@Inject('email') private  email) {
-    console.log('this.store', this.email);
+  constructor(@Inject('EVENT_STORE') private eventBus: any) {
+    console.log('this.eventBus', this.eventBus);
+  }
+  @Get('/v1')
+  getUser() {
+    return `SALAM every one`;
   }
 }
